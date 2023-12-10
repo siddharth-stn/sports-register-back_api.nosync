61b89e581e06ee686b65604e979090464db60003
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-12-10 13:54:46 +0530

    group routes created

--- routes/sportregister.js
+++ routes/sportregister.js
@@ -3,6 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 const sessionController = require("../controllers/sessionController");
+const groupController = require("../controllers/groupController");
 
 // Session Routes
 router.get("/sessions", sessionController.session_list);
@@ -12,5 +13,14 @@ router.post("session/create", sessionController.session_create_post);
 router.post("session/:id/update", sessionController.session_update_post);
 
 router.post("session/:id/delete", sessionController.session_delete_post);
+
+// Group Routes
+router.get("groups", groupController.group_list);
+
+router.post("group/create", groupController.group_create_post);
+
+router.post("group/:id/update", groupController.group_update_post);
+
+router.post("group/:id/delete", groupController.group_delete_post);
 
 module.exports = router;
