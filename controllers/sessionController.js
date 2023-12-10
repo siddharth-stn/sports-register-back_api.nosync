87517aefdd7adda26289b78105d8025d186dbb74
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-12-10 13:49:58 +0530

    Created Routes for session

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -10,6 +10,11 @@ exports.session_create_post = (req, res, next) => {
   res.send("Not Implemented: Session Create on POST");
 };
 
+// Update a Session
+exports.session_update_post = (req, res, next) => {
+  res.send("Not Implemented: Session Update on POST");
+};
+
 // Delete a Session
 exports.session_delete_post = (req, res, next) => {
   res.send("Not Implemented: Session Delete on POST");
--- routes/sportregister.js
+++ routes/sportregister.js
@@ -2,5 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
+const sessionController = require("../controllers/sessionController");
+
 // Session Routes
-router.get();
+router.get("/sessions", sessionController.session_list);
+
+router.post("session/create", sessionController.session_create_post);
+
+router.post("session/:id/update", sessionController.session_update_post);
+
+router.post("session/:id/delete", sessionController.session_delete_post);
+
+module.exports = router;
