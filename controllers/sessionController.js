57420c87ba73157a21585cf1381478dc88622163
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-12-10 13:17:28 +0530

    Session Controller Created

---
+++ controllers/sessionController.js
@@ -0,0 +1,16 @@
+const Session = require("../models/session-model");
+
+// Display list of Sessions
+exports.session_list = (req, res, next) => {
+  res.send("Not Implemented: Session List");
+};
+
+// Create a Session
+exports.session_create_post = (req, res, next) => {
+  res.send("Not Implemented: Session Create Post");
+};
+
+// Delete a Session
+exports.session_delete_post = (req, res, next) => {
+  res.send("Not Implemented: Session Delete Post");
+};
---
+++ routes/sportregister.js
@@ -0,0 +1,6 @@
+const express = require("express");
+
+const router = express.Router();
+
+// Session Routes
+router.get();
