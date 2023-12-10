050739e08134cd21fd45923f072e2f0a1f19bf66
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-12-10 20:01:54 +0530

    written session create controller

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,3 +1,5 @@
+const asyncHandler = require("express-async-handler");
+
 const Session = require("../models/session-model");
 
 const { body, validationResult } = require("express-validator");
@@ -10,14 +12,38 @@ exports.session_list = (req, res, next) => {
 // Handle Session Create on POST
 exports.session_create_post = [
   // Validate and Sanitize the Field
-  body("session", "Session format YYYY-YY").trim().escape(),
-  async (req, res, next) => {
+  body("session", "Session format YYYY-YY")
+    .trim()
+    .notEmpty()
+    .withMessage("Session can not be Empty")
+    .matches(/^\d{4}-\d{2}$/)
+    .withMessage("Format should be YYYY-YY")
+    .escape(),
+  asyncHandler(async (req, res, next) => {
     // Extract the validation errors from request.
     const errors = validationResult(req);
 
     // Create a session object with escaped and trimmed data
     const session = new Session({ session: req.body.session });
-  },
+
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    } else {
+      // Data is valid
+      // Check if the similar session already exists
+      const sessionExists = await Session.findOne({
+        session: req.body.session,
+      }).exec();
+
+      if (sessionExists) {
+        const err = new Error("Session Exists in the Database");
+        res.json({ messgae: err.message });
+      } else {
+        await session.save();
+        //! To write the code to redirect to Group Creation Page after this
+      }
+    }
+  }),
 ];
 
 // Update a Session
