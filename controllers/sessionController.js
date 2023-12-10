69b86bd79404af4dc192a5f8f389f3ec7523ce67
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-12-10 18:04:59 +0530

    working on session controller create

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,14 +1,24 @@
 const Session = require("../models/session-model");
 
+const { body, validationResult } = require("express-validator");
+
 // Display list of Sessions
 exports.session_list = (req, res, next) => {
   res.send("Not Implemented: Session List");
 };
 
-// Create a Session
-exports.session_create_post = (req, res, next) => {
-  res.send("Not Implemented: Session Create on POST");
-};
+// Handle Session Create on POST
+exports.session_create_post = [
+  // Validate and Sanitize the Field
+  body("session", "Session format YYYY-YY").trim().escape(),
+  async (req, res, next) => {
+    // Extract the validation errors from request.
+    const errors = validationResult(req);
+
+    // Create a session object with escaped and trimmed data
+    const session = new Session({ session: req.body.session });
+  },
+];
 
 // Update a Session
 exports.session_update_post = (req, res, next) => {
