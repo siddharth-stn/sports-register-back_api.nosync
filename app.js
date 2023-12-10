840eafe85809930a06d204093a19e38ec10f72b8
Siddharth Pande <sidarjunstn.sp@gmail.com>
2023-12-10 14:58:25 +0530

    minor bugs removed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var sportregisterRouter = require("./routes/sportregister");
 
 var app = express();
 
@@ -18,6 +19,7 @@ const mongoDB =
 main()
   .then(() => console.log("Database Connected Successfully"))
   .catch((err) => console.log(err));
+
 async function main() {
   await mongoose.connect(mongoDB);
 }
@@ -34,6 +36,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/sportregister/", sportregisterRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/sportregister.js
+++ routes/sportregister.js
@@ -8,19 +8,19 @@ const groupController = require("../controllers/groupController");
 // Session Routes
 router.get("/sessions", sessionController.session_list);
 
-router.post("session/create", sessionController.session_create_post);
+router.post("/session/create", sessionController.session_create_post);
 
-router.post("session/:id/update", sessionController.session_update_post);
+router.post("/session/:id/update", sessionController.session_update_post);
 
-router.post("session/:id/delete", sessionController.session_delete_post);
+router.post("/session/:id/delete", sessionController.session_delete_post);
 
 // Group Routes
-router.get("groups", groupController.group_list);
+router.get("/groups", groupController.group_list);
 
-router.post("group/create", groupController.group_create_post);
+router.post("/group/create", groupController.group_create_post);
 
-router.post("group/:id/update", groupController.group_update_post);
+router.post("/group/:id/update", groupController.group_update_post);
 
-router.post("group/:id/delete", groupController.group_delete_post);
+router.post("/group/:id/delete", groupController.group_delete_post);
 
 module.exports = router;
